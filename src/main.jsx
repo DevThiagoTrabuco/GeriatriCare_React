079eb8b3854531ae50b426d8e1d5c19eda3ee121
DevThiagoTrabuco <thiago.m.lima@ba.estudante.senai.br>
2025-12-08 13:16:08 -0300

    fix: add tailwindcss

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import { router } from './routes/index.jsx'
 
+import './main.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
